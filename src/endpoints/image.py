82c0d1adef405e6c59c2e69641ed5c6eac43af04
nicfro <nicolai.frost@gmail.com>
2021-03-07 22:19:55 +0100

    added image functionality

--- src/endpoints/image.py
+++ src/endpoints/image.py
@@ -6,16 +6,18 @@ from ..schemas.images import ImageSchema, ImageCreate
 from ..orm_models.db_models import ImageModel
 from . import DBC
 from src.logic.hasher import Hasher
+import base64
 
 
 router = APIRouter()
 #, file: UploadFile = File(...)
-@router.post("/uploadfile/")
+@router.post("/images")
 async def post_image(user_id: str = Form(...), file: UploadFile = File(...), db: Session = Depends(DBC.get_session)):
     """
     POST one image
-    It reads parameters from the request field and add missing fields from default values defined in the model
-    :param user: ImageBase class that contains all columns in the table
+    Gets user_id from form and file as an UploadFile object
+    :param user_id: the user id of the image
+    :param file: the file to be uploaded
     :param db: DB session
     :return: Created image entry
     """
@@ -28,5 +30,25 @@ async def post_image(user_id: str = Form(...), file: UploadFile = File(...), db:
     db.commit()
     db.refresh(image_model)
     return {"message": f"Image created with id: {image_model.id}"}
+
+
+@router.get("/images/id/{image_id}", response_model=ImageSchema)
+async def get_image_by_id(image_id: str, db: Session = Depends(DBC.get_session)):
+    """
+    GET one image 
+    Get image_id from form
+    :param image_id: identifier for the image to be returned
+    :param db: DB session
+    :return: image as binary data
+    """
+    try:
+        # Get user by name
+        image = db.query(ImageModel).filter(ImageModel.id == image_id).one()
+        return {"id": image.id,
+                "user_id": image.user_id,
+                "file": base64.b64encode(image.file), 
+                "created_at": image.created_at}
+    except sqlalchemy.orm.exc.NoResultFound:
+        raise Exception(f"{image_id} does not exist")
 
 
